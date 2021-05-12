ca34242fa1ddf80de1735efd8c9499662a6360fc
Vitaliy <theyamshikov@yandex.ru>
2021-05-12 23:34:13 +0600

    upd

---
+++ src/Bundle.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ZnTool\Stress;
+
+use ZnCore\Base\Libs\App\Base\BaseBundle;
+
+class Bundle extends BaseBundle
+{
+
+    public function console(): array
+    {
+        return [
+            'ZnTool\Stress\Commands',
+        ];
+    }
+
+    public function container(): array
+    {
+        return [
+            __DIR__ . '/Domain/config/container.php',
+        ];
+    }
+}
---
+++ src/Domain/config/container.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\Console\Application;
+use ZnCore\Base\Libs\DotEnv\DotEnv;
+use Illuminate\Container\Container;
+use ZnLib\Db\Factories\ManagerFactory;
+use ZnLib\Db\Capsule\Manager;
+use ZnTool\Stress\Domain\Repositories\Conf\ProfileRepository;
+
+return [
+    'definitions' => [],
+    'singletons' => [
+        Application::class => Application::class,
+        Manager::class => function () {
+            return ManagerFactory::createManagerFromEnv();
+        },
+        ProfileRepository::class => function () {
+            if(empty($_ENV['STRESS_PROFILE_CONFIG'])) {
+                throw new \ZnCore\Base\Exceptions\InvalidConfigException('Empty ENV "STRESS_PROFILE_CONFIG"!');
+            }
+            $configFileName = __DIR__ . '/../../../../../../' . $_ENV['STRESS_PROFILE_CONFIG'];
+            $config = include ($configFileName);
+            return new ProfileRepository($config);
+        },
+    ],
+];
