96994c88b313b6eec4e74f486f17bc058d27274d
ZnDeveloper <zndev@ya.ru>
2022-06-20 23:36:59 +0600

    upd

--- src/Domain/Repositories/Conf/ProfileRepository.php
+++ src/Domain/Repositories/Conf/ProfileRepository.php
@@ -10,7 +10,7 @@ use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
 use ZnCore\Base\Libs\Validation\Exceptions\UnprocessibleEntityException;
 use ZnCore\Base\Libs\Entity\Helpers\EntityHelper;
 use ZnCore\Contract\Domain\Interfaces\Entities\EntityIdInterface;
-use ZnCore\Domain\Interfaces\Repository\CrudRepositoryInterface;
+use ZnCore\Base\Libs\Repository\Interfaces\CrudRepositoryInterface;
 use ZnCore\Base\Libs\Query\Entities\Query;
 use ZnTool\Dev\Runtime\Domain\Helpers\Benchmark;
 use ZnTool\Stress\Domain\Entities\ProfileEntity;
