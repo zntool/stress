78f1a0cd8aaea84b32a584a1941aa27bfb21e93b
Vitaliy <theyamshikov@yandex.ru>
2022-06-21 11:51:55 +0600

    upd

--- src/Domain/Domain.php
+++ src/Domain/Domain.php
@@ -2,7 +2,7 @@
 
 namespace ZnTool\Stress\Domain;
 
-use ZnCore\Domain\Interfaces\DomainInterface;
+use ZnCore\Base\Libs\Domain\Interfaces\DomainInterface;
 
 class Domain implements DomainInterface
 {
