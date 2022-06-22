85dfe59a7abe057b840daf5508ead3e49defef07
Vitaliy <theyamshikov@yandex.ru>
2022-06-22 18:34:07 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -1,7 +1,7 @@
 <?php
 
 use Symfony\Component\Console\Application;
-use ZnCore\Base\Libs\DotEnv\DotEnv;
+use ZnCore\Base\Libs\DotEnv\Domain\Libs\DotEnv;
 use Illuminate\Container\Container;
 use ZnDatabase\Eloquent\Domain\Factories\ManagerFactory;
 use ZnDatabase\Eloquent\Domain\Capsule\Manager;
